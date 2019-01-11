a5d9ef3ce2e689aadcb8f23586f760f2aa7b70d0
Fanny <fanisitavalverde@gmail.com>
2019-01-11 01:52:43 -0500

    cambios

--- Base.sql
+++ Base.sql
@@ -5,13 +5,30 @@ CREATE TABLE servicio(
 	beneficios varchar(100)
 );
 
+CREATE TABLE brochure(
+	id serial NOT NULL PRIMARY KEY,
+	nombre varchar(20),
+	fecha date,
+	servicio_id int NOT NULL,
+	FOREIGN KEY(servicio_id) REFERENCES servicio(id)
+);
+
+CREATE TABLE oficina(
+	id int NOT NULL PRIMARY KEY,
+	nombre varchar(30),
+	telefono varchar(20),
+	direccion varchar(50)
+);
+
 CREATE TABLE vendedor(
 	id serial NOT NULL PRIMARY KEY,
 	nombre varchar(20),
 	apellido varchar(20),
 	cedula varchar(10),
 	telefono varchar(20),
-	correo varchar(100)
+	correo varchar(100),
+	oficina_id int NOT NULL,
+	FOREIGN KEY(oficina_id) REFERENCES oficina(id)
 );
 
 CREATE TABLE empresa(
@@ -42,7 +59,7 @@ CREATE TABLE solicitud(
 	FOREIGN KEY(empresa_id) REFERENCES empresa(ruc)
 );
 
-CREATE TABLE comentarios(
+CREATE TABLE noticia(
 	id serial NOT NULL PRIMARY KEY,
 	titulo varchar(100),
 	descripcion varchar(500),
@@ -52,3 +69,23 @@ CREATE TABLE comentarios(
 	usuario_id int NOT NULL,
 	FOREIGN KEY(usuario_id) REFERENCES usuario(id)
 );
+
+CREATE TABLE comentario(
+	id serial NOT NULL PRIMARY KEY,
+	detalle varchar(500),
+	fecha date,
+	usuario_id int NOT NULL,
+	FOREIGN KEY(usuario_id) REFERENCES usuario(id)
+);
+
+CREATE TABLE contrato(
+	id serial NOT NULL PRIMARY KEY,
+	fecha date,
+	servicio_id int NOT NULL,
+	vendedor_id int NOT NULL,
+	empresa_id varchar(15) NOT NULL,
+	FOREIGN KEY(servicio_id) REFERENCES servicio(id),
+	FOREIGN KEY(empresa_id) REFERENCES empresa(ruc),
+	FOREIGN KEY(vendedor_id ) REFERENCES vendedor(id)
+);
+
--- Segtium/Segtium/settings.py
+++ Segtium/Segtium/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'Segtium.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'segtiumSA',
+        'USER': 'django_windows',
+        'PASSWORD': '1997',
+        'HOST': 'localhost',
+        'PORT':'',
     }
 }
 
