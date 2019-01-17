b75629cfea8e8807b84b0ae2254b7264c8d73258
ivinces <ivinces@espol.edu.ec>
2019-01-16 19:26:55 -0500

    Admin

--- Segtium/paginas/admin.py
+++ Segtium/paginas/admin.py
@@ -1,3 +1,47 @@
-from django.contrib import admin
+from __future__ import unicode_literals 
 
-# Register your models here.
+from django.contrib import admin 
+
+from .models import Noticias,Servicio,Brochure,Vendedor,Empresa,Beneficios
+
+class NoticiaAdmin(admin.ModelAdmin): 
+	ordering = ('-id',) 
+
+	search_fields = ('titulo', 'descripcion','fuente','imagen',) 
+
+
+class ServicioAdmin(admin.ModelAdmin):
+	ordering = ('-id',) 
+
+	search_fields = ('nombre', 'descripcion') 
+		
+
+class BrochureAdmin(admin.ModelAdmin):
+	ordering = ('-id',) 
+
+	search_fields = ('nombre','fecha') 
+
+
+class VendedorAdmin(admin.ModelAdmin):
+	ordering = ('-id',) 
+
+	search_fields = ('nombre', 'apellido','cedula','telefono','correo') 
+
+
+class EmpresaAdmin(admin.ModelAdmin):
+	ordering = ('-id',) 
+
+	search_fields = ('ruc','nombre', 'telefono','direccion','representante')
+
+
+class BeneficiosAdmin(admin.ModelAdmin):
+	ordering = ('-id',) 
+
+	search_fields = ('nombre', 'descripcion')  
+
+admin.site.register(Noticias, NoticiaAdmin)
+admin.site.register(Servicio, ServicioAdmin)
+admin.site.register(Brochure, BrochureAdmin) 
+admin.site.register(Vendedor, VendedorAdmin) 
+admin.site.register(Empresa, EmpresaAdmin)
+admin.site.register(Beneficios, BeneficiosAdmin)  
