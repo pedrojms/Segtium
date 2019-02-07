f76373596dc9000fae3ebcf06b18470a4ec83cc4
Fanny <fmvalver@espol.edu.ec>
2019-02-06 21:21:52 -0500

    centrado

--- Segtium/paginas/controller.py
+++ Segtium/paginas/controller.py
@@ -43,14 +43,10 @@ def Add(registro):
 	return response
 
 def Delete(dato):
-<<<<<<< HEAD
 	response=requests.delete('http://127.0.0.1:8001/noticias/'+dato+"/")
 	return response
-=======
-  response=requests.delete('http://127.0.0.1:8001/noticias/'+dato+"/")
-  return response
 
 def InfoPerfil(correo):
   model=Usuario.objects.raw("SELECT pu.id,pu.nombre,pu.apellido,pu.correo FROM paginas_usuario pu WHERE pu.correo='"+correo+"'")
   return model
->>>>>>> 65587c2ff66ac3f339c99eab85bada60e46cb63d
+
